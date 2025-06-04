0f34b47e6978dfb79c39aef2d668adcd680ea619
Raul Sousa <raulsousadesigner@gmail.com>
2025-06-04 12:57:32 -0300

    Add: Another type of data visualization

--- src/graphics.py
+++ src/graphics.py
@@ -1,20 +1,27 @@
 from scipy.stats import kurtosis # type: ignore
 import matplotlib.pyplot as plt # type: ignore
-#from matplotlib.ticker import FuncFormatter # type: ignore
+from matplotlib.ticker import FuncFormatter # type: ignore
 import seaborn as sns # type: ignore
 import numpy as np
 
-def analisarEPlotar(dados, titulo, tipo):
+def analisarEPlotar(dados, titulo, tipo, y):
     dados_np = np.array(dados, dtype=np.float64)  # converte explicitamente para float
     curt = kurtosis(dados_np)
 
     plt.figure(figsize=(8, 5))
-    sns.histplot(dados_np, kde=True, color='skyblue', bins=30, stat='density')
+    
+    if (y) == 'S':
+        plt.gca().yaxis.set_major_formatter(FuncFormatter(lambda y, _: f'{y * 100:.0f}%'))
+        sns.histplot(dados_np, kde=True, color='skyblue', bins=30, stat='probability')
+        plt.ylabel('Percentual de dados: %')
+    else:
+        sns.histplot(dados_np, kde=True, color='skyblue', bins=30, stat='density')
+        plt.ylabel('Densidade')
+        
     plt.axvline(np.mean(dados_np), color='red', linestyle='--', label='Média')
-    # plt.gca().yaxis.set_major_formatter(FuncFormatter(lambda y, _: f'{y * 100:.0f}%')) // para mostrar percentual de dados
     plt.title(f'{titulo}\nCurtose: {curt:.2f}')
     plt.xlabel(tipo)
-    plt.ylabel('Densidade')
+    
     plt.legend()
     plt.grid(True)
     plt.tight_layout()
--- src/utils/mainUtils.py
+++ src/utils/mainUtils.py
@@ -10,20 +10,26 @@ def verDados(data,nome):
         input("\n Digite qualquer entrada para continuar: ")
     print(" ")
 
+def verPercentual():
+    key = input(f'Deseja ver em percentual? S/N: ').upper()
+    return key
+
 def switch(dadosVendas, dadosClima, dadosFinancas):
     print('1 - Analisar e plotar dados de vendas')
     print('2 - Analisar e plotar dados de clima')
     print('3 - Analisar e plotar dados de financas')
     print(" ")
-    
+
     key = int(input("Digite uma opção: "))
+    
 
     while (key == 1 or key == 2 or key==3):
+        ylabel = verPercentual()
         if key == 1:
-            analisarEPlotar(dadosVendas, "Loja", "Unidades")
+            analisarEPlotar(dadosVendas, "Loja", "Unidades", ylabel)
         elif key == 2:
-            analisarEPlotar(dadosClima, "Clima", "Temperatura °C")
+            analisarEPlotar(dadosClima, "Clima", "Temperatura °C", ylabel)
         elif key == 3:
-            analisarEPlotar(dadosFinancas, "Bolsa de valores", "Retornos")
+            analisarEPlotar(dadosFinancas, "Bolsa de valores", "Retornos", ylabel)
 
         key = int(input("Digite uma opção: "))
